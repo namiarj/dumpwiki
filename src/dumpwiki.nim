bfdffd10536fbdf19c2034eea7f0d52895769eae
Nami Arj <namiarjmandi@gmail.com>
2022-06-29 10:13:08 +0000

    Add files via upload

--- src/dumpwiki.nim
+++ src/dumpwiki.nim
@@ -1,8 +1,10 @@
 import parsecfg, strformat, os
 import procs
 
+const cfg_default = "config.ini"
+
 let 
-  cfg = loadConfig("config.ini")
+  cfg = loadConfig(cfg_default)
   sitemap = cfg.getSectionvalue("mediawiki", "sitemap")
   baseurl = cfg.getSectionvalue("mediawiki", "baseurl")
 
@@ -10,8 +12,25 @@ var file = fetchSitemap(sitemap)
 let articles = importSitemap(file)
 removeFile(file)
 
-var counter = 1
+var counter = 0 
+var skip = ' '
 for article in articles:
-  echo(fmt"{counter}/{articles.len} {article}")
-  file = fetchArticle(baseurl, article)
-  counter.inc
+  block jump:
+    counter.inc
+    if fileExists(article & ".xml"):
+      case skip
+      of ' ':
+        echo(fmt"The file {article}.xml already exists")
+        echo("Skip? a (all)/n (none)/o (once)")
+        skip = stdin.readChar()
+      of 'a': break jump 
+      of 'n': discard
+      of 'o':
+        skip = ' '
+        break jump
+      else: skip = ' '
+    echo(fmt"{counter}/{articles.len} {article}")
+    try:
+      file = fetchArticle(baseurl, article)
+    except:
+      quit(fmt"error fetching {article}")
